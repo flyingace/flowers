66e6fa70ed8770d1fb6002db1a3b33ec7f7eee05
David Cameron <dc@davidcameron.biz>
2019-07-27 01:45:46 -0400

    Improved upon next/previous links (somewhat)

--- src/components/Poem/Poem.jsx
+++ src/components/Poem/Poem.jsx
@@ -1,9 +1,12 @@
+/* eslint no-use-before-define: off */
+
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import './Poem.scss';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import './Poem.scss';
 
+/* Poem */
 const Poem = (props) => {
   const {
     getPoemById,
@@ -32,35 +35,56 @@ const Poem = (props) => {
     if (poemTitle !== '') {
       setIsFading(true);
     }
-  }, [setIsFading, poemTitle]);
+  }, [poemId, poemTitle]);
 
-  const generatePoemBody = () => {
-    return poemBody.map((line, idx) => {
-      // eslint-disable-next-line react/no-array-index-key
-      return <Line key={`line${idx}`}>{line}</Line>;
+  const generatePoemBody = (lines, sectionNumber = '') => {
+    if (typeof lines[0] === 'string') {
+      return lines.map((line, idx) => {
+        // eslint-disable-next-line react/no-array-index-key
+        return <Line key={`${sectionNumber}line${idx}`}>{line}</Line>;
+      });
+    }
+
+    return poemBody.map((section, idx) => {
+      const { poemSection } = section;
+      return (
+        // eslint-disable-next-line react/no-array-index-key
+        <React.Fragment key={`${idx}section${poemSection.sectionNumber}`}>
+          <div className="poem-section-title">{poemSection.sectionNumber}</div>
+          <PoemBody>
+            {generatePoemBody(
+              poemSection.sectionBody,
+              poemSection.sectionNumber
+            )}
+          </PoemBody>
+        </React.Fragment>
+      );
     });
   };
 
   const poemClass = isFading ? 'poem fade-in' : 'poem';
   return (
-    <div className={poemClass}>
-      <div className="poem-number">{poemNumber}</div>
-      <Title>{poemTitle}</Title>
-      {poemSubTitle && <div className="poem-subtitle">{poemSubTitle}</div>}
-      {poemEpigram && <div className="poem-epigram">{poemEpigram}</div>}
-      {poemDedication && (
-        <div className="poem-dedication">{poemDedication}</div>
-      )}
-      <PoemBody>{generatePoemBody()}</PoemBody>
-      <Link to={ROUTES.POEMEDIT}>edit</Link>
-    </div>
+    <React.Fragment>
+      <div className={poemClass}>
+        <div className="poem-number">{poemNumber}</div>
+        <Title>{poemTitle}</Title>
+        {poemSubTitle && <div className="poem-subtitle">{poemSubTitle}</div>}
+        {poemEpigram && <div className="poem-epigram">{poemEpigram}</div>}
+        {poemDedication && (
+          <div className="poem-dedication">{poemDedication}</div>
+        )}
+        <PoemBody>{generatePoemBody(poemBody)}</PoemBody>
+        <Link to={ROUTES.POEMEDIT}>edit</Link>
+      </div>
+    </React.Fragment>
   );
 };
 
 Poem.propTypes = {
   getPoemById: PropTypes.func.isRequired,
-  match: PropTypes.objectOf(PropTypes.any).isRequired,
-  poemBody: PropTypes.arrayOf(PropTypes.string).isRequired,
+  poemBody: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+  ).isRequired,
   poemDedication: PropTypes.string,
   poemEpigram: PropTypes.string,
   poemId: PropTypes.string,
@@ -74,13 +98,15 @@ Poem.propTypes = {
 Poem.defaultProps = {
   poemDedication: '',
   poemEpigram: '',
-  poemId: 'benediction',
+  poemId: '',
   poemNumber: '',
   poemSubTitle: '',
 };
 
 export default Poem;
+/*  */
 
+/* Title */
 export const Title = ({ children }) => {
   return <div className="poem-title">{children}</div>;
 };
@@ -88,7 +114,9 @@ export const Title = ({ children }) => {
 Title.propTypes = {
   children: PropTypes.node.isRequired,
 };
+/*  */
 
+/* Poem Body */
 export const PoemBody = ({ children }) => {
   return <div className="poem-body">{children}</div>;
 };
@@ -100,7 +128,9 @@ PoemBody.propTypes = {
 PoemBody.defaultProps = {
   children: <></>,
 };
+/*  */
 
+/* Line */
 export const Line = ({ children }) => {
   const line = children.split('');
   line.splice(line.lastIndexOf(' '), 1, '\xa0');
@@ -112,3 +142,4 @@ export const Line = ({ children }) => {
 Line.propTypes = {
   children: PropTypes.node.isRequired,
 };
+/*  */
