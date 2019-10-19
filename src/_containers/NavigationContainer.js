2f0df4bf3fad5c34921ebd86a38ed73c21d8719f
David Cameron <dc@davidcameron.biz>
2019-10-18 23:47:03 -0400

    Last and first poems do not display "next" and "previous" links respectively. Also, navigation data is derived from TOCData selector.

--- src/_containers/NavigationContainer.js
+++ src/_containers/NavigationContainer.js
@@ -2,10 +2,12 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as PoemsActions from '../_actions/PoemsActions';
 import Navigation from '../components/Navigation/Navigation';
+import { TOCData } from '../_selectors/selectors';
 
 function mapStateToProps(state) {
   return {
     poemId: state.PoemData.targetPoem.poemId,
+    tocData: TOCData(state),
   };
 }
 
